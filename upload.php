47a237ae7d44695967104791dfaf7ee7f93ec91a
Pedro Molina <pedrom@pekebyte.com>
2013-01-29 15:07:05 -0430

    php file update
    Doing a basic upload of file

--- upload.php
+++ upload.php
@@ -7,21 +7,13 @@ Copyright (c) 2013 Pedro Molina
 // Define a destination
 $targetFolder = '/uploads'; // Relative to the root
 
-
-if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
-	$tempFile = $_FILES['file']['tmp_name'];
-	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
-	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['file']['name'];
-	
-	// Validate the file type
-	$fileTypes = array('jpg','jpeg','gif','png'); // File extensions
-	$fileParts = pathinfo($_FILES['file']['name']);
-	
-	if (in_array($fileParts['extension'],$fileTypes)) {
-		move_uploaded_file($tempFile,$targetFile);
-		echo '1';
-	} else {
-		echo 'Invalid file type.';
-	}
+if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFolder . $_FILES['file']['name']))
+{
+    print "File uploaded";
+} 
+else
+{
+    print "Error";
 }
+
 ?>
