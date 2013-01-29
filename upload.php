254c3e5ddb20a20fe5f70f3c298e7ddb59239c71
Pedro Molina <pedrom@pekebyte.com>
2013-01-29 15:08:52 -0430

    php upload

--- upload.php
+++ upload.php
@@ -7,13 +7,21 @@ Copyright (c) 2013 Pedro Molina
 // Define a destination
 $targetFolder = '/uploads'; // Relative to the root
 
-if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFolder . $_FILES['file']['name']))
-{
-    print "File uploaded";
-} 
-else
-{
-    print "Error";
-}
 
+if (!empty($_FILES)) {
+	$tempFile = $_FILES['file']['tmp_name'];
+	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
+	$targetFile = rtrim($targetPath,'/') . '/' . $_FILES['file']['name'];
+	
+	// Validate the file type
+	$fileTypes = array('jpg','jpeg','gif','png'); // File extensions
+	$fileParts = pathinfo($_FILES['file']['name']);
+	
+	if (in_array($fileParts['extension'],$fileTypes)) {
+		move_uploaded_file($tempFile,$targetFile);
+		echo '1';
+	} else {
+		echo 'Invalid file type.';
+	}
+}
 ?>
