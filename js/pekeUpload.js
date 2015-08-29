449c828a394fb1b09482ac5749f76f1b03340dfb
Pedro Molina <pamolinaf@gmail.com>
2015-08-28 22:01:19 -0430

    Handling drag & drop events

--- js/pekeUpload.js
+++ js/pekeUpload.js
@@ -51,6 +51,48 @@
       container: null,
       init : function(){
         this.replacehtml();
+        $(document).on('click','.pkuparea',function(){
+          pekeUpload.selectfiles();
+        });
+        ///Handle events when drag
+        if (options.dragSupport){
+          var dragarea = this.obj.next('div.pkdragarea');
+          $(document).on('dragenter', function (e) 
+          {
+            e.stopPropagation();
+            e.preventDefault();
+          });
+          $(document).on('dragover', function (e) 
+          {
+            e.stopPropagation();
+            e.preventDefault();
+          });
+          $(document).on('drop', function (e) 
+          {
+            e.stopPropagation();
+            e.preventDefault();
+          });
+          dragarea.on('dragenter', function (e) 
+          {
+            e.stopPropagation();
+            e.preventDefault();
+            $(this).css('border', '2px solid #0B85A1');
+          });
+          dragarea.on('dragover', function (e) 
+          {
+            e.stopPropagation();
+            e.preventDefault();
+          });
+          dragarea.on('drop', function (e) 
+          {
+            $(this).css('border', '2px dotted #0B85A1');
+            e.preventDefault();
+            var files = e.originalEvent.dataTransfer.files;
+            for (var i = 0; i < files.length; i++){
+              pekeUpload.files.push(files[i]);
+            }
+          });
+        }
       },
       replacehtml: function(){
         var html = null;
@@ -58,20 +100,20 @@
           case true:
             switch(options.bootstrap){
               case true:
-                html = '<div class="well well-lg" style="cursor:pointer"><h4>'+options.dragText+'</h4></div>';
+                html = '<div class="well well-lg pkuparea pkdragarea" style="cursor:pointer"><h4>'+options.dragText+'</h4></div>';
               break;
               case false:
-                html = '<div class="pekeupload-drag-area" syle="cursor:pointer"><h4>'+options.dragText+'</h4></div>';
+                html = '<div class="pekeupload-drag-area pkuparea pkdragarea" syle="cursor:pointer"><h4>'+options.dragText+'</h4></div>';
               break;
             }
           break;
           case false:
             switch(options.bootstrap){
               case true:
-                html = '<a href="javascript:void(0)" class="btn btn-primary btn-upload"> <i class="glyphicon glyphicon-upload"></i> '+options.btnText+'</a><div class="pekecontainer"></div>';
+                html = '<a href="javascript:void(0)" class="btn btn-primary btn-upload pkuparea"> <i class="glyphicon glyphicon-upload"></i> '+options.btnText+'</a><div class="pekecontainer"></div>';
               break;
               case false:
-                html = '<a href="javascript:void(0)" class="pekeupload-btn-file">'+options.btnText+'</a><div class="pekecontainer"></div>';
+                html = '<a href="javascript:void(0)" class="pekeupload-btn-file pkuparea">'+options.btnText+'</a><div class="pekecontainer"></div>';
               break;
             }
           break;
@@ -80,6 +122,12 @@
         this.obj.hide();
         this.container = this.obj.next('div.pekecontainer');
       },
+      selectfiles: function(){
+        this.obj.click();
+      },
+      handlefile: function(file){
+
+      }
     };
     
     //Do the magic
