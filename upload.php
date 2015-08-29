646f5766a49a8eb024516619dd44b10b77c1c72e
Pedro Molina <pamolinaf@gmail.com>
2015-08-29 18:23:03 -0430

    Set a JSON Response to upload.php

--- upload.php
+++ upload.php
@@ -18,12 +18,12 @@ if (!empty($_FILES)) {
 	$fileParts = pathinfo($_FILES['file']['name']);
 	$response = array ();
 	if (in_array($fileParts['extension'],$fileTypes)) {
-		// move_uploaded_file($tempFile,$targetFile);
-		// $response['success'] = 1;
-		// foreach ($_POST as $key => $value){
-		// 	$response[$key] = $value;
-		// }
-		echo 1;
+		move_uploaded_file($tempFile,$targetFile);
+		$response['success'] = 1;
+		foreach ($_POST as $key => $value){
+			$response[$key] = $value;
+		}
+		echo json_encode($response);
 	} else {
 		$response['success'] = 0;
 		$response['error'] = 'Invalid file type.';
